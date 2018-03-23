78fe4f6336c5ddc9433b0f1eb3b4236566ec21dc
Jared Gabel <Fubukiyo@gmail.com>
2018-03-23 08:07:18 -0500

    Formatting issues and updated executable.

--- printConsole/Program.cs
+++ printConsole/Program.cs
@@ -24,7 +24,7 @@ namespace printConsole
                 input = Console.ReadLine();
                 //Console.WriteLine("How many labels?");
                 //choice = Console.Read();
-                toPrint = "${" + Environment.NewLine + "CT~~CD,~CC^~CT~" + Environment.NewLine + "^XA~TA000~JSN^LT0^MNW^MTD^PON^PMN^LH0,0^JMA^PR2,2~SD30^JUS^LRN^CI0^XZ" + Environment.NewLine + "^XA" + Environment.NewLine + "^MMT" + Environment.NewLine + "^PW609" + Environment.NewLine + "^LL0203" + Environment.NewLine + "^LS-14" + Environment.NewLine + "^BY2,3,50^FT85,150^BCN,,N,N" + Environment.NewLine + "^fd>" + input + "^fs" + Environment.NewLine + "^FT75,65^A0N,40,40^FH" + Environment.NewLine + "^FD" + input + "^fs" +Environment.NewLine + "^PQ2,0,1,Y^XZ" + Environment.NewLine + "}$";
+                toPrint = "${" + Environment.NewLine + "CT~~CD,~CC^~CT~" + Environment.NewLine + "^XA~TA000~JSN^LT0^MNW^MTD^PON^PMN^LH0,0^JMA^PR2,2~SD30^JUS^LRN^CI0^XZ" + Environment.NewLine + "^XA" + Environment.NewLine + "^MMT" + Environment.NewLine + "^PW609" + Environment.NewLine + "^LL0203" + Environment.NewLine + "^LS-14" + Environment.NewLine + "^BY2,3,50^FT85,150^BCN,,N,N" + Environment.NewLine + "^fd>:" + input + "^fs" + Environment.NewLine + "^FT75,65^A0N,40,40^FH\\" + Environment.NewLine + "^FD" + input + "^fs" +Environment.NewLine + "^PQ2,0,1,Y^XZ" + Environment.NewLine + "}$";
                 using (System.IO.StreamWriter file = new System.IO.StreamWriter("label.txt", true))
                 {
                     file.WriteLine(toPrint);
