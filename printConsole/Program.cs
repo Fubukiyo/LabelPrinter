8623a98cf0d852d7819b18006dd00bcd88ee6b8d
Jared Gabel <Fubukiyo@gmail.com>
2018-03-23 12:44:24 -0500

    Changed how the program prints

--- printConsole/Program.cs
+++ printConsole/Program.cs
@@ -4,8 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Management.Automation;
+using System.Drawing.Printing;
 using System.IO;
+using System.Printing;
 using System.Management.Automation.Runspaces;
+using System.Diagnostics;
 
 namespace printConsole
 {
@@ -13,8 +16,6 @@ namespace printConsole
     {
         static void Main(string[] args)
         {
-            //Variables
-            //int choice;
             string input = null;
             string toPrint;
 
@@ -22,20 +23,28 @@ namespace printConsole
             {
                 Console.WriteLine("Scan your barcode:");
                 input = Console.ReadLine();
-                //Console.WriteLine("How many labels?");
-                //choice = Console.Read();
                 toPrint = "${" + Environment.NewLine + "CT~~CD,~CC^~CT~" + Environment.NewLine + "^XA~TA000~JSN^LT0^MNW^MTD^PON^PMN^LH0,0^JMA^PR2,2~SD30^JUS^LRN^CI0^XZ" + Environment.NewLine + "^XA" + Environment.NewLine + "^MMT" + Environment.NewLine + "^PW609" + Environment.NewLine + "^LL0203" + Environment.NewLine + "^LS-14" + Environment.NewLine + "^BY2,3,50^FT85,150^BCN,,N,N" + Environment.NewLine + "^fd>:" + input + "^fs" + Environment.NewLine + "^FT75,65^A0N,40,40^FH\\^FD" + input + "^fs" +Environment.NewLine + "^PQ2,0,1,Y^XZ" + Environment.NewLine + "}$";
+
+                System.IO.File.WriteAllText("label.txt", toPrint);
+                
+                /*
                 using (System.IO.StreamWriter file = new System.IO.StreamWriter("label.txt", true))
                 {
                     file.WriteLine(toPrint);
                 }
-                Console.ReadKey();
+                */
+
+                System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo("label.txt");
+                psi.Verb = "print";
+
+                Process.Start(psi);
+                /*
                 PowerShell shell = PowerShell.Create();
                 shell.AddCommand("Get-Content");
                 shell.AddArgument("label.txt");
                 shell.AddCommand("Out-Printer");
                 shell.Invoke();
-                File.Delete("label.txt");
+                */
             }
         }
     }
